8b2c07ce50762c0b6e04774c6c681e6e3828efe1
SoroushTayebi <89043336+SoroushTayebi@users.noreply.github.com>
2023-10-30 02:13:02 -0400

    Epic 5 + tests

--- epic1/test_inCollege.py
+++ epic1/test_inCollege.py
@@ -2,6 +2,7 @@ import unittest
 import io
 from inCollege import *
 from contextlib import redirect_stdout
+from io import StringIO
 import mock
 from unittest.mock import patch
 
@@ -128,6 +129,110 @@ class TestInCollege(unittest.TestCase):
         with mock.patch('inCollege.ALL_STUDENT_ACCOUNTS',  {"wyoming44":{'university':"SJSU","lastName":"Vo", "firstName":"Jason", "major":"CS", 'requests': []}, "wyoming55":{'university':"SJSU","lastName":"Tran", "firstName":"Kevin", "major":"CS", 'requests': []}}) as mocked_find:
             find()
             assert mocked_find['wyoming44']['requests'] == ['wyoming55']
+
+
+    #test if the profile major can be changed
+    @patch('inCollege.globalUsername', 'wyoming55')
+    @patch('builtins.input', side_effect=['2', 'CS', '8'])
+    @patch('inCollege.ALL_STUDENT_ACCOUNTS',  {"wyoming44":{'university':"SJSU","lastName":"Vo", "firstName":"Jason", "major":"CS", 'requests': []}, "wyoming55":{'university':"SJSU","lastName":"Tran", "firstName":"Kevin", "major":"CS", 'requests': []}})
+    def test_change_major(self, mock_input):
+        out = StringIO()
+        with patch('sys.stdout', out):
+            profileChange()
+
+        expected_output = "New Title"
+        self.assertIn(expected_output, out.getvalue())
+
+    #test if the profile university can be changed
+    @patch('inCollege.globalUsername', 'wyoming55')
+    @patch('builtins.input', side_effect=['3', 'New University', '8'])
+    @patch('inCollege.ALL_STUDENT_ACCOUNTS',  {"wyoming44":{'university':"SJSU","lastName":"Vo", "firstName":"Jason", "major":"CS", 'requests': []}, "wyoming55":{'university':"SJSU","lastName":"Tran", "firstName":"Kevin", "major":"CS", 'requests': []}})
+    def test_change_university(self, mock_input):
+        out = StringIO()
+        with patch('sys.stdout', out):
+            profileChange()
+
+        expected_output = "New University"
+        self.assertIn(expected_output, out.getvalue())
+
+    #test if the profile "new about section" can be changed
+    @patch('inCollege.globalUsername', 'wyoming55')
+    @patch('builtins.input', side_effect=['4', 'New About Section', '8'])
+    @patch('inCollege.ALL_STUDENT_ACCOUNTS',  {"wyoming44":{'university':"SJSU","lastName":"Vo", "firstName":"Jason", "major":"CS", 'requests': []}, "wyoming55":{'university':"SJSU","lastName":"Tran", "firstName":"Kevin", "major":"CS", 'requests': []}})
+    def test_change_about_section(self, mock_input):
+        out = StringIO()
+        with patch('sys.stdout', out):
+            profileChange()
+
+        expected_output = "New About Section"
+        self.assertIn(expected_output, out.getvalue())
+
+
+
+    #Test to view friends profile 
+    @patch('inCollege.globalUsername', 'wyoming55')
+    @patch('inCollege.ALL_STUDENT_ACCOUNTS', {
+        "wyoming44": {'university': "SJSU", 'lastName': "Vo", 'firstName': "Jason", 'major': "CS", 'requests': []},
+        "wyoming56": {'university': "SJSU", 'lastName': "Smith", 'firstName': "Alice", 'major': "Engineering", 'requests': []},
+    })
+    def test_view_friend_profile(self):
+        # Simulate a friend relationship
+        ALL_STUDENT_ACCOUNTS['wyoming55']['friends'] = ['wyoming44', 'wyoming56']
+
+        # Redirect the function's output to capture it
+        out = StringIO()
+        with patch('sys.stdout', out):
+            viewFriendsProfiles()
+
+        # Verify that the friend's profile is correctly displayed
+        expected_output = "Alice Smith's Profile:\n"
+        self.assertIn(expected_output, out.getvalue())
+        self.assertIn("Title: ", out.getvalue())
+        self.assertIn("Major: Engineering", out.getvalue())
+        self.assertIn("University: SJSU", out.getvalue())
+        self.assertIn("About: ", out.getvalue())
+        self.assertIn("Education: ", out.getvalue())
+
+    #Test to view other non-friends profile
+    @patch('inCollege.globalUsername', 'wyoming55')
+    @patch('inCollege.ALL_STUDENT_ACCOUNTS', {
+        "wyoming44": {'university': "SJSU", 'lastName': "Vo", 'firstName': "Jason", 'major': "CS", 'requests': []},
+    })
+    def test_view_non_friend_profile(self):
+        # Simulate no friend relationship
+        ALL_STUDENT_ACCOUNTS['wyoming55']['friends'] = []
+
+        # Redirect the function's output to capture it
+        out = StringIO()
+        with patch('sys.stdout', out):
+            viewFriendsProfiles()
+
+        # Verify that the function correctly handles non-friends
+        expected_output = "You have not added any friends yet"
+        self.assertIn(expected_output, out.getvalue())
+
+    #Test Case to display list of friends
+    @patch('inCollege.globalUsername', 'wyoming55')
+    @patch('inCollege.ALL_STUDENT_ACCOUNTS', {
+        "wyoming44": {'university': "SJSU", 'lastName': "Vo", 'firstName': "Jason", 'major': "CS", 'requests': []},
+        "wyoming56": {'university': "SJSU", 'lastName': "Smith", 'firstName': "Alice", 'major': "Engineering", 'requests': []},
+    })
+    def test_display_friends_list(self):
+        # Simulate friend relationships
+        ALL_STUDENT_ACCOUNTS['wyoming55']['friends'] = ['wyoming44', 'wyoming56']
+
+        # Redirect the function's output to capture it
+        out = StringIO()
+        with patch('sys.stdout', out):
+            showMyNetwork()
+
+        # Verify that the list of friends is displayed
+        expected_output = "1. Jason Vo, SJSU, CS"
+        self.assertIn(expected_output, out.getvalue())
+        expected_output = "2. Alice Smith, SJSU, Engineering"
+        self.assertIn(expected_output, out.getvalue())
+
+
 
 if __name__ == '__main__':
     unittest.main()
